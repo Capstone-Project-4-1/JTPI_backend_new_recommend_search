ed31294435ba58c72057e23e874e151242d6596e
MinGiAnnnngJin <mkj03096@naver.com>
2024-05-20 22:15:41 +0900

    완료됐을껄 아마

--- src/main/java/com/example/demo/Controller/PassController.java
+++ src/main/java/com/example/demo/Controller/PassController.java
@@ -4,7 +4,6 @@ import com.example.demo.DTO.PassSearchResultDTO;
 import com.example.demo.SearchParameters;
 import com.example.demo.Service.PassService;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
